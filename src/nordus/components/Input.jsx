bfd7e1ecadcdab4fe211e36342ff63de6ebbf30b
Anna-Carla <annacarlateixeira16@gmail.com>
2024-04-02 11:29:19 -0300

    feat: transforming the input into a component

---
+++ src/nordus/components/Input.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { TextInput, StyleSheet } from "react-native";
+import Colors from "../assets/util/Colors";
+
+const Input = ({ placeholder, placeholderTextColor, secureTextEntry, onChangeText }) => {
+  return (
+    <TextInput
+      style={styles.input}
+      placeholder={placeholder}
+      placeholderTextColor={placeholderTextColor || Colors.SILVER}
+      secureTextEntry={secureTextEntry}
+      onChangeText={onChangeText}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    width: 281,
+    height: 54,
+    borderRadius: 18,
+    fontSize: 16,
+    backgroundColor: Colors.DARKER_GRAY,
+    paddingLeft: 25,
+    marginBottom: 3,
+  },
+});
+
+export default Input;
