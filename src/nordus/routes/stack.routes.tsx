08ddf182038da9d148f02953a7cbb7c111bdc2db
Anna-Carla <annacarlateixeira16@gmail.com>
2024-04-02 20:41:52 -0300

    fix: conflict

--- src/nordus/routes/stack.routes.tsx
+++ src/nordus/routes/stack.routes.tsx
@@ -1,13 +1,13 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const {Screen, Navigator} = createNativeStackNavigator ();
-import Login from '../screens/Login'
+//import Login from '../screens/Login'
 import Register from '../screens/Register'
 
 export function StackRoutes () {
     return (
         <Navigator>
-            <Screen name='Login' component={Login}/>
+            {/**<Screen name='Login' component={Login}/> */}
             <Screen name='Register' component={Register}/>
         </Navigator>
     )
