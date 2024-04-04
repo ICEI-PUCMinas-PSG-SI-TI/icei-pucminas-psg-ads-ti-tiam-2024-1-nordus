79cca4cc6a9290b6c9cf32b3ed86f380c8dafc82
arttturslv <arttturslv@gmail.com>
2024-04-04 14:44:07 -0300

    refactor: Changed where the slide timer logic is located
    The logic was on the father component (home) and now its on self slider component

--- src/nordus/components/Slide.jsx
+++ src/nordus/components/Slide.jsx
@@ -1,33 +1,47 @@
 import { useState } from "react";
-import { StyleSheet, View, Image, FlatList, Pressable, Text } from "react-native";
 
-export default function Slide({index, arr}) {
+import { StyleSheet, View, Image, FlatList} from "react-native";
+import NordusIcon from '../assets/icons/icon-nordus-white.svg'
+
+export default function Slide({timing, arr}) {
+  const [index, setIndex] = useState([0]);
+
+  function changeIndex() {
+    var i = index;
+    var newIndex = (i+1) % arr.length;
+     setIndex(newIndex);
+  }
+
+  loop(timing, changeIndex);
+
+  function loop(timing, func) {
+    if(timing==0)
+    return;
+    setTimeout(func, timing)
+  }
 
     return (
-        <Pressable style={styles.container}>
+        <View style={styles.container}>
+          
           <FlatList
-          ListEmptyComponent={<Image style={{width:430, height:230}} source={{uri:'https://cdn5.vectorstock.com/i/1000x1000/73/49/404-error-page-not-found-miss-paper-with-white-vector-20577349.jpg'}}/>}
-          keyExtractor={(item) => item.id}
-          data={arr}
-          renderItem={({item}) => (
-            item.id == index 
-            ?
-              <View key={item.id} style={styles.slider}>
-                <Image style={{width:430, height:230}} source={{uri:item.url}}/>
-              </View>
-            :
-              null
-          )}
-          />
-                    <View style={styles.bulletIndicator}>
+            ListEmptyComponent={<NordusIcon height={230} width={430}/>}
+            data={arr}
+            renderItem={({item}) => ( item.id == index 
+              ? <View key={item.id} style={styles.slider}>
+                  <Image style={{width:430, height:230}} source={{uri:item.url}}/>
+                </View>
+              : null
+          )}/>
+            
+          <View style={styles.bulletIndicator}>
             {arr.map((item, indexMap) => (
-              index==indexMap?
-              <View  style={[styles.dot, {backgroundColor:'#EA714C'}]}></View>
-              :
-              <View  style={styles.dot}></View>
+              index==indexMap
+              ? <View  style={[styles.dot, {backgroundColor:'#EA714C'}]}></View>
+              : <View  style={styles.dot}></View>
             ))}
           </View>
-        </Pressable>
+
+        </View>
     );
 }
 
