228b17e17a46e79d4fa40ca5a7063118cbb340fa
arttturslv <arttturslv@gmail.com>
2024-04-05 20:08:09 -0300

    feat(router): Added drawer navigation

---
+++ src/nordus/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+import Menu from '../assets/icons/icon-menu.svg'
+import Logo from '../assets/nordus-horizontal.png'
+import Colors from '../assets/util/Colors';
+
+const Header = ({ navigation }) => {
+  const openMenu = () => {
+    navigation.openDrawer();
+  };
+  return (
+    <View style={styles.header}>
+      <TouchableOpacity onPress={openMenu} style={styles.icon}>
+        <Menu height={14} width={22}/>
+      </TouchableOpacity>
+      <View style={styles.headerImage}>
+        <Image source={Logo}/>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 12,
+    height: 64,
+    flexDirection: "row",
+    justifyContent: "center",
+    backgroundColor: Colors.WHITE_SMOKE
+  },
+  headerImage: {
+    alignSelf: 'center'
+  },
+  icon: {
+    left: 16,
+    alignSelf: 'center',
+    position: 'absolute',
+    padding: 4
+  }
+});
+
+export default Header;
--- src/nordus/routes/index.jsx
+++ src/nordus/routes/index.jsx
@@ -1,11 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import {StackRoutes} from "./stack.routes"
+import {DrawerRoutes} from "./drawer.routes"
 
 
 export function Routes () {
     return (
         <NavigationContainer>
-            <StackRoutes/>
+            <DrawerRoutes/>
         </NavigationContainer>
     )
 }
