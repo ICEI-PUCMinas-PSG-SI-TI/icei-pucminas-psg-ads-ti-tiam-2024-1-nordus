5bd0e2c4b4fc477fa457b37bd1910ba86554f030
AR3X <alexgdiniz18@gmail.com>
2024-04-12 20:10:04 -0300

    Add Tab Routes

--- src/nordus/routes/tab.routes.jsx
+++ src/nordus/routes/tab.routes.jsx
@@ -1,11 +1,20 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
-import { SvgUri } from 'react-native';
-import { Header } from "react-native/Libraries/NewAppScreen";
+import Login from "../screens/Login";
+import Register from "../screens/Register";
+
+
+import IconCalendar from "../assets/icons/icon-calendar-black.svg"
+import IconCalendarPressed from "../assets/icons/icon-calendar-tangerine.svg"
+import IconDiamond from "../assets/icons/icon-diamond-black.svg"
+import IconDiamondPressed from "../assets/icons/icon-diamond-tangerine.svg"
 import IconProfile from "../assets/icons/icon-profile.svg"
+import IconProfilePressed from "../assets/icons/icon-profile-tangerine.svg"
 import IconHome from "../assets/icons/icon-home.svg"
 import IconHomePressed from "../assets/icons/icon-home-tangerine.svg"
+
+
 import Colors from "../assets/util/Colors";
 const Tab = createBottomTabNavigator();
 
@@ -35,9 +44,43 @@ export default function TabRoutes(){
                 }
             }}
             />
+            <Tab.Screen
+            name="Login"
+            component={Login}
+            options={{
+                tabBarIcon:({focused}) => {
+                    if(focused){
+                        return <IconCalendarPressed/>
+                    }
+                    return <IconCalendar/>
+                }
+            }}
+            />
+             <Tab.Screen
+            name="Register"
+            component={Register}
+            options={{
+                tabBarIcon:({focused}) => {
+                    if(focused){
+                        return <IconDiamondPressed/>
+                    }
+                    return <IconDiamond/>
+                }
+            }}
+            />
+
+            
               <Tab.Screen
             name="Profile"
             component={Profile}
+            options={{
+                tabBarIcon:({focused}) => {
+                    if(focused){
+                        return <IconProfilePressed/>
+                    }
+                    return <IconProfile/>
+                }
+            }}
             />
         </Tab.Navigator>
     )
