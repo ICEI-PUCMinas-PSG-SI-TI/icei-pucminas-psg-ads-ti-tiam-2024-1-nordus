a11e7d78cd771db4d59208919fd6fc9777130c41
arttturslv <arttturslv@gmail.com>
2024-04-20 20:20:52 -0300

    refac: Changed the way that images are imported and fixed some styles details

--- src/nordus/components/Header.jsx
+++ src/nordus/components/Header.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
 import Menu from '../assets/icons/icon-menu.svg'
-import Logo from '../assets/nordus-horizontal.png'
+import Logo from '../assets/nordus-horizontal.svg'
 import Colors from '../assets/util/Colors';
 
 const Header = ({ navigation }) => {
   const openMenu = () => {
     navigation.openDrawer();
   };
+
   return (
     <View style={styles.header}>
       <TouchableOpacity onPress={openMenu} style={styles.icon}>
         <Menu height={14} width={22}/>
       </TouchableOpacity>
       <View style={styles.headerImage}>
-        <Image source={Logo}  style={{width: 110, height: 30}} />
+        <Logo style={{width: 120, height: 40}}/>
       </View>
     </View>
   );
@@ -22,8 +23,8 @@ const Header = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   header: {
-    marginTop: 12,
-    height: 64,
+    marginTop: 18,
+    height: 60,
     flexDirection: "row",
     justifyContent: "center",
     backgroundColor: Colors.WHITE_SMOKE
@@ -35,7 +36,7 @@ const styles = StyleSheet.create({
     left: 16,
     alignSelf: 'center',
     position: 'absolute',
-    padding: 4
+    paddingLeft: 4,
   }
 });
 
--- src/nordus/screens/Home.jsx
+++ src/nordus/screens/Home.jsx
@@ -1,13 +1,7 @@
-import { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  FlatList,
-  Pressable,
-} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Linking, Pressable, TouchableHighlight} from "react-native";
+
 import Colors from "../assets/util/Colors";
 
 import Divider from "../components/Divider";
@@ -15,39 +9,64 @@ import Slide from "../components/Slide";
 
 import Clock from "../assets/icons/icon-clock";
 import Location from "../assets/icons/icon-location.svg";
-
 import Instagram from "../assets/icons/icon-instagram.svg";
 import Website from "../assets/icons/icon-website.svg";
 import Whatsapp from "../assets/icons/icon-whatsapp.svg";
 import Email from "../assets/icons/icon-email.svg";
 
 export default function Home() {
-  const [slider, setSlider] = useState([
+  const navigation = useNavigation();
+
+  const slider = [
     {
       id: 0,
-      url: "https://plus.unsplash.com/premium_photo-1677444546739-21b8aad351d4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      url: require("../assets/slider/1.jpg"),
     },
     {
       id: 1,
-      url: "https://images.unsplash.com/photo-1599351431202-1e0f0137899a?q=80&w=988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      url: require("../assets/slider/2.jpg"),
     },
     {
       id: 2,
-      url: "https://images.unsplash.com/photo-1536520002442-39764a41e987?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      url: require("../assets/slider/3.jpg"),
     },
     {
       id: 3,
-      url: "https://images.unsplash.com/photo-1621605815971-fbc98d665033?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      url: require("../assets/slider/4.jpg"),
     },
     {
       id: 4,
-      url: "https://images.unsplash.com/photo-1596728325488-58c87691e9af?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      url: require("../assets/slider/5.jpg"),
     },
-  ]);
+    {
+      id: 5,
+      url: require("../assets/slider/6.jpg"),
+    },
+    {
+      id: 6,
+      url: require("../assets/slider/7.jpg"),
+    },
+    {
+      id: 7,
+      url: require("../assets/slider/8.jpg"),
+    },
+  ];
+
+  const openURL = async (url) => {
+    const suporta = await Linking.canOpenURL(url);
+    if(suporta) {
+        await Linking.openURL(url);
+    } else {
+        Alert.alert('nao sei abrir')
+    }
+}
 
   return (
     <SafeAreaView style={styles.container}>
-
+    <StatusBar
+      barStyle="dark-content"
+      backgroundColor="#f5f5f5"
+      />
       <Slide arr={slider}/>
 
       <View style={styles.contentInformation}>
@@ -78,16 +97,24 @@ export default function Home() {
         <Divider color={Colors.DARKER_GRAY} />
 
         <View style={styles.icons}>
-          <Instagram height={24} width={24} />
-          <Website height={24} width={24} />
-          <Whatsapp height={24} width={24} />
-          <Email height={26} width={26} />
+          <Pressable onPress={() => openURL('https://www.instagram.com/barbearianordus/')}>
+            <Instagram height={24} width={24}/>
+          </Pressable>
+          <Pressable onPress={() => openURL('https://barbearianordus.com.br/')}>
+            <Website height={24} width={24}/>
+          </Pressable>
+          <Pressable onPress={() => openURL('https://wa.me/553131916557?text=Estou+afim+de+marcar+um+hor%C3%A1rio+para+dar+um+trato+no+cabelo%2Fbarba.+Cheguei+at%C3%A9+voc%C3%AAs+pelo+app+e+t%C3%B4+bem+empolgado+pra+conhecer+de+perto%21')}>
+            <Whatsapp height={24} width={24}/>
+          </Pressable>
+          <Pressable onPress={ () => openURL('mailto:barbearianordus@gmail.com?subject=Marcar%20um%20hor%C3%A1rio&body=Estou%20afim%20de%20marcar%20um%20hor%C3%A1rio%20para%20dar%20um%20trato%20no%20cabelo%2Fbarba.%20Cheguei%20at%C3%A9%20voc%C3%AAs%20pelo%20app%20e%20t%C3%B4%20bem%20empolgado%20pra%20conhecer%20de%20perto!%0D%0A%0D%0A')}>
+            <Email height={26} width={26}/>
+          </Pressable>
         </View>
       </View>
 
-      <View style={styles.button}>
+      <TouchableHighlight underlayColor='#d96541' style={styles.button} onPress={()=> {navigation.navigate('Tab', { screen: 'Assinaturas' })}}>
         <Text style={styles.buttonText}>Agendar</Text>
-      </View>
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
@@ -97,14 +124,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "top",
+    backgroundColor: Colors.BLACK,
+    paddingBottom: 27,
   },
   contentInformation: {
     flex: 1,
     width: "100%",
     backgroundColor: Colors.BLACK,
     paddingHorizontal: 22,
-    paddingTop: 18,
-    gap: 18,
+    paddingTop: 12,
+    gap: 12,
   },
   title: {
     fontSize: 20,
@@ -125,21 +154,19 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     gap: 18,
+    paddingTop: 5,
   },
   button: {
     backgroundColor: Colors.TANGERINE,
     height: 50,
     width: "50%",
     borderRadius: 20,
-    position: "absolute",
-    bottom: 30,
-    zIndex: 50,
-    flex: 1,
     justifyContent: "center",
   },
   buttonText: {
     fontWeight: "400",
     fontSize: 20,
     textAlign: "center",
+    color: Colors.BLACK
   },
 });
