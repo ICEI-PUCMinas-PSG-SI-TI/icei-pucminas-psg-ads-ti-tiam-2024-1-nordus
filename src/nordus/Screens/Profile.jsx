12d01ae900b04b9868046104a5f60429f2864990
arttturslv <arttturslv@gmail.com>
2024-05-05 17:27:05 -0300

    style: changed small things

--- src/nordus/Screens/Profile.jsx
+++ src/nordus/Screens/Profile.jsx
@@ -8,7 +8,8 @@ import Exit from '../assets/icons/exit-icon.svg'
 
 export default function Profile() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={{flex: 1, backgroundColor: Colors.BLACK}} >
+      <View style={styles.container}>
       <Ads></Ads>
       <View style={styles.options}>
         <View style={styles.header}>
@@ -58,7 +59,8 @@ export default function Profile() {
         <Divider color={"#353535"}/>
 
       </View>
-      
+      </View>
+
       <View style={styles.aboutApp}>
         <Text style={styles.aboutAppText}>
           Desenvolvido por Triad Software.
@@ -81,9 +83,10 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
     backgroundColor: Colors.BLACK,
-    paddingVertical: 50,
+    paddingVertical: 20,
     paddingHorizontal: 20,
-    gap: 30
+    gap: 20,
+    position: 'relative'
   },
   header: {
     flexDirection:'row',
@@ -95,28 +98,28 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   aboutApp:{
-    position: 'absolute',
     color: '#fff',
     bottom: 16,
     flexDirection: 'row',
-    gap: 30,
     alignItems: 'flex-end',
-    justifyContent: 'center',
-    alignSelf: 'center'
+    justifyContent: 'space-around',
+    bottom: 10,
+    backgroundColor: Colors.BLACK,
   },  
   aboutAppText: {
     color: '#fff', 
     fontWeight: '100', 
-    fontSize: 12
+    fontSize: 12,
   },
   title: {
     color: '#fff',
     fontSize: 22,
-    fontWeight: '500'
+    fontWeight: '500',
   },
   subtitle: {
     color: '#fff',
-    fontSize: 16
+    fontSize: 16,
+    paddingBottom: 4
   },
   description: {
     color: '#fff',
