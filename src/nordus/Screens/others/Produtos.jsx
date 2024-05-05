3efd7fb32fa430b1ace9bdb44e35d749b4412973
arttturslv <arttturslv@gmail.com>
2024-05-05 13:30:15 -0300

    feat: added product page

--- src/nordus/Screens/others/Produtos.jsx
+++ src/nordus/Screens/others/Produtos.jsx
@@ -1,24 +1,49 @@
 import React from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
-
+import { SafeAreaView, ScrollView, Text, StyleSheet, Image, View } from "react-native";
+import Colors from "../../assets/util/Colors";
+import ProductItem from "../../components/ProductItem";
 export default function Produto() {
   return (
-    <SafeAreaView style={styles.container} >
-      <Text style={styles.text}>Produtos</Text>
-    </SafeAreaView>
+    <ScrollView style={styles.container} >
+      <Text style={styles.title}>Produtos</Text>
+      
+      <View style={styles.products}>
+          <ProductItem name="Pomada Capilar King" price="20,00"></ProductItem>
+          <ProductItem name="Pomada Capilar King" price="20,00"></ProductItem>
+          <ProductItem name="Pomada Capilar King" price="20,00"></ProductItem>
+          <ProductItem name="Pomada Capilar King" price="20,00"></ProductItem>
+          <ProductItem name="Pomada Capilar King" price="20,00"></ProductItem>
+          <ProductItem name="Pomada Capilar King" price="20,00"></ProductItem>
+          <ProductItem name="Pomada Capilar King" price="20,00"></ProductItem>
+
+      </View>
+      
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#2e2e2e',
+    backgroundColor: Colors.BLACK,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: "center"
+ 
   },
-  text: {
-    fontSize: 42,
-    color: '#fff'
-  }
+  title: {
+    fontSize: 28,
+    fontWeight: '300',
+    color: '#fff',
+    marginTop: 90,
+    paddingVertical: 30,
+    paddingLeft: 55,
+    alignSelf: 'flex-start'
+  },
+  products: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 25,
+    flex: 1,
+    justifyContent: 'center'
+  },
+
 })
  
