c6dae9ccfdc34cc160fdf21cbb53710565bb242b
Davi Ribeiro <davirpa1@gmail.com>
2024-05-12 20:00:50 -0300

    feat: persistência do usuário logado, com async storage

--- src/nordus/screens/Login.jsx
+++ src/nordus/screens/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   View,
@@ -12,21 +12,53 @@ import Colors from "../assets/util/Colors";
 import Input from "../components/Input";
 import Logo from "../components/Logo";
 import { useNavigation } from "@react-navigation/native";
+import { auth } from "../FirebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function Login() {
+export default function Login({ setIsUserLoggedIn }) {
   const [formData, setFormData] = useState({
     login: "",
     password: "",
   });
-
+  const [loading, setLoading] = useState(false);
   const navigation = useNavigation();
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
+  const checkLoginStatus = async () => {
+    try {
+      const userToken = await AsyncStorage.getItem("userToken");
+      if (userToken) {
+        setIsUserLoggedIn(true);
+      }
+    } catch (error) {
+      console.log("Erro ao verificar status de login:", error);
+    }
+  };
 
   const handleInputChange = (key, value) => {
     setFormData({ ...formData, [key]: value });
   };
 
-  const handleLogin = () => {
-    navigation.navigate("Home");
+  const handleLogin = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(
+        auth,
+        formData.login,
+        formData.password
+      );
+      await AsyncStorage.setItem("userToken", response.user.uid);
+      setIsUserLoggedIn(true);
+    } catch (error) {
+      console.log("Erro ao fazer login:", error);
+      Alert.alert("Erro de Login", "Por favor, verifique seu email e senha.");
+    } finally {
+      setLoading(false);
+    }
   };
 
   const navigateCadastro = () => {
@@ -127,8 +159,5 @@ const styles = StyleSheet.create({
   },
   boldText: {
     fontWeight: "bold",
-  },
-  linkItem: {
-    marginTop: 10,
   },
 });
