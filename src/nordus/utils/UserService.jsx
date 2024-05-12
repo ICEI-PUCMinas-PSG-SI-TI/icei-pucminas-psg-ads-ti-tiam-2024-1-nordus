22a8271f642a7e9fe54c5832ab49ca4be5a557a9
Davi Ribeiro <davirpa1@gmail.com>
2024-05-12 20:03:23 -0300

    feat: funcionalidade para buscar um usuário a partir de seu ID
    foi adicionada um arquivo pra módulos relacionados a serviços de usuário, nesse momento foi implementada a função getUser, que irá buscar um usuário no banco de dados com base em seu ID

---
+++ src/nordus/utils/UserService.jsx
@@ -0,0 +1,30 @@
+import { query, where, collection, getDocs } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FIREBASE_DB } from "../FirebaseConfig";
+import { auth } from "../FirebaseConfig";
+
+export const getUser = async (id) => {
+  try {
+    const usersCollection = collection(FIREBASE_DB, "users");
+    const q = query(usersCollection, where("id", "==", id));
+    const queryResposne = await getDocs(q);
+
+    if (!queryResposne.empty) {
+      const user = queryResposne.docs[0].data();
+      return user;
+    } else {
+      console.log("Nenhum documento encontrado para o ID fornecido: " + id);
+      return null;
+    }
+  } catch (error) {
+    console.log("Erro ao obter usuário:", error);
+    throw error;
+  }
+};
+
+//TODO: Implementar função de logout do usuário
+export const logoutUser = async () => {
+  await AsyncStorage.removeItem("userToken");
+  await auth.signOut();
+  setIsUserLoggedIn(false);
+};
