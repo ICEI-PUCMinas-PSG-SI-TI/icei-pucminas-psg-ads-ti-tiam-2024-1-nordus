563defb20cc83940ef21afb4b7cb65e7c66f32bd
Anna-Carla <annacarlateixeira16@gmail.com>
2024-06-01 16:46:54 -0300

    fix(wip): appointments request and permission firebase error

--- src/nordus/screens/others/AgendamentosAdicional.jsx
+++ src/nordus/screens/others/AgendamentosAdicional.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, Text, StyleSheet, View, ScrollView, Pressable } from "react-native";
-
+import { SafeAreaView, Text, StyleSheet, View, Pressable } from "react-native";
 import Barber from "../../components/Barber";
 import Hour from "../../components/Hour";
 import Calendar from "../../components/Calendar";
@@ -9,49 +8,40 @@ import { getAppointments } from "../../utils/AppointmentService";
 import { FIREBASE_DB } from "../../FirebaseConfig";
 import { collection, getDocs, query, where } from "firebase/firestore";
 
-export default function AgendamentoAdicional({duration}) {
-
+export default function AgendamentoAdicional({ duration }) {
   const [barbers, setBarbers] = useState([]);
   const [barbeiroEscolhido, setBarbeiroEscolhido] = useState(null);
   const [data, setData] = useState(null);
   const [horario, setHorario] = useState(null);
 
   const getBarbers = async () => {
-    
     try {
-
       let barbersAsync = await AsyncStorage.getItem("barbers");
-      var barbersArr = barbersAsync ? JSON.parse(barbersAsync) : null;
-  
-      if(!barbersArr) {
+      let barbersArr = barbersAsync ? JSON.parse(barbersAsync) : null;
 
-        console.log("buscando barbeiros.")
+      if (!barbersArr) {
+        console.log("buscando barbeiros.");
         const usersCollection = collection(FIREBASE_DB, "users");
         const q = query(usersCollection, where("barber", "==", true));
         const queryResponse = await getDocs(q);
-  
+
         if (!queryResponse.empty) {
-          barbersArr =  [];
-          queryResponse.docs.map(doc => 
-            {
-              let dt = doc.data();
-              barbersArr.push(dt);
-              doc.data();
-            }
-            );
+          barbersArr = [];
+          queryResponse.docs.forEach((doc) => {
+            let dt = doc.data();
+            barbersArr.push(dt);
+          });
+          await AsyncStorage.setItem("barbers", JSON.stringify(barbersArr));
           setBarbers(barbersArr);
         } else {
-            console.log("Nenhum barbeiro encontrado");
-          return null;
+          console.log("Nenhum barbeiro encontrado");
         }
       } else {
-        console.log("barbeiros já estão armazenados.")
-        console.log(barbers)
-        return barbers;
+        console.log("barbeiros já estão armazenados.");
+        setBarbers(barbersArr);
       }
     } catch (error) {
       console.log("Erro ao obter barbeiros:", error);
-      throw error;
     }
   };
 
@@ -60,44 +50,64 @@ export default function AgendamentoAdicional({duration}) {
   }, []);
 
   useEffect(() => {
-    try {
-      getAppointments(barbeiroEscolhido, data);
-    } catch (error) {
-      console.log(error)
+    if (barbeiroEscolhido && data) {
+      try {
+        getAppointments(barbeiroEscolhido, data).then((appointments) => {
+          console.log("Compromissos obtidos:", appointments);
+        });
+      } catch (error) {
+        console.log("Erro ao obter compromissos:", error);
+      }
     }
-  },[data]);
-
- 
+  }, [barbeiroEscolhido, data]);
 
   return (
-    <SafeAreaView style={styles.container} >
-      <View style={{gap:12}}>
-        <Text style={{color: '#fff', fontSize: 24}}>Escolha um profissional:</Text>
-        <View style={{flexDirection: 'row', gap: 20}}>
-            {
-              barbers.map((barber, index) => (
-                <Pressable key={index} onPress={() => 
-                  {
-                    setBarbeiroEscolhido(barber.id);
-                    console.log(JSON.stringify(barber.id))
-                  }}>
-                  <Barber name={barber.name} escolhido={barbeiroEscolhido==barber.id? true: false} uri={barber.imageURL} />
-                </Pressable>
-              ))
-            } 
+    <SafeAreaView style={styles.container}>
+      <View style={{ gap: 12 }}>
+        <Text style={{ color: "#fff", fontSize: 24 }}>
+          Escolha um profissional:
+        </Text>
+        <View style={{ flexDirection: "row", gap: 20 }}>
+          {barbers.map((barber, index) => (
+            <Pressable
+              key={index}
+              onPress={() => {
+                setBarbeiroEscolhido(barber.id);
+                console.log(JSON.stringify(barber.id));
+              }}
+            >
+              <Barber
+                name={barber.name}
+                escolhido={barbeiroEscolhido === barber.id}
+                uri={barber.imageURL}
+              />
+            </Pressable>
+          ))}
         </View>
       </View>
 
-      <View style={{gap: 24}}>
-          <View style={{gap:12, height: 370, overflow: 'hidden', borderBottomEndRadius: 20, borderBottomLeftRadius: 20}}>
-              <Text style={{color: '#fff', fontSize: 20}}>Escolha uma data: </Text>
-              <Calendar setData={setData}/>
-          </View>
+      <View style={{ gap: 24 }}>
+        <View
+          style={{
+            gap: 12,
+            height: 370,
+            overflow: "hidden",
+            borderBottomEndRadius: 20,
+            borderBottomLeftRadius: 20,
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 20 }}>
+            Escolha uma data:{" "}
+          </Text>
+          <Calendar setData={setData} />
+        </View>
 
-          <View style={{gap:12}}>
-              <Text style={{color: '#fff', fontSize: 20}}>Escolha uma horario: </Text>
-              <Hour setHorario={setHorario}/>
-          </View>
+        <View style={{ gap: 12 }}>
+          <Text style={{ color: "#fff", fontSize: 20 }}>
+            Escolha um horário:{" "}
+          </Text>
+          <Hour setHorario={setHorario} />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -105,7 +115,7 @@ export default function AgendamentoAdicional({duration}) {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#1A1E21',
+    backgroundColor: "#1A1E21",
     flex: 1,
     alignItems: "left",
     paddingTop: 20,
@@ -114,7 +124,6 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 42,
-    color: '#fff'
-  }
-})
- 
+    color: "#fff",
+  },
+});
--- src/nordus/utils/AppointmentService.jsx
+++ src/nordus/utils/AppointmentService.jsx
@@ -2,32 +2,35 @@ import { query, where, collection, getDocs, and } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { FIREBASE_DB } from "../FirebaseConfig";
 
-// saber quais horarios estao disponiveis:
-// Get no appointments
-// Get no appointments da data selecionada e que o status esteja valido.
-// Verifico o tipo de servico (duracao) e o horario. (horario+duracao = tempo indisponivel)
-// -> verificacoes do horario de trabalho - tempos indisponiveis.
-
+// [ ] saber quais horarios estao disponiveis:
+// [X] Get no appointments
+// [ ] Get no appointments da data selecionada e que o status esteja valido.
+// [ ] Verifico o tipo de servico (duracao) e o horario. (horario+duracao = tempo indisponivel)
+// [ ]-> verificacoes do horario de trabalho - tempos indisponiveis.
 
 export const getAppointments = async (barberID, dataEscolhida) => {
-    var appointments;
-    console.log("barberID", barberID)
-    console.log("dataEscolhida", dataEscolhida)
-
   try {
-      const usersCollection = collection(FIREBASE_DB, "appointments");
-      const q = query(usersCollection);
-      const queryResponse = await getDocs(q);
-    
-      if (!queryResponse.empty) {
-        appointments = queryResponse.docs[0].data();
-        return appointments;
-      } else {
-        console.log("Nenhum documento encontrado para o ID fornecido: " + id);
-        return null;
-      }
-    } catch (error) {
-        console.log("Erro:", error);
-        throw error;
+    console.log("barberID", barberID);
+    console.log("dataEscolhida", dataEscolhida);
+
+    const appointmentsCollection = collection(FIREBASE_DB, "appointments");
+    const q = query(
+      appointmentsCollection,
+      where("barberID", "==", barberID),
+      where("date", "==", dataEscolhida)
+    );
+
+    const queryResponse = await getDocs(q);
+
+    if (!queryResponse.empty) {
+      const appointments = queryResponse.docs.map(doc => doc.data());
+      return appointments;
+    } else {
+      console.log("Nenhum agendamento encontrado para o barbeiro e data fornecidos.");
+      return [];
     }
+  } catch (error) {
+    console.log("Erro ao obter agendamentos:", error);
+    throw error;
+  }
 };
