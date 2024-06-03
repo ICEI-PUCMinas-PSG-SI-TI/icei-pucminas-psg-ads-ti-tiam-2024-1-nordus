16e444da4df5fd59919df103cc39aa3bc4161337
Anna-Carla <annacarlateixeira16@gmail.com>
2024-06-02 22:35:49 -0300

    feat(wip): addDoc appointments in the firebase

--- src/nordus/screens/others/AgendamentosAdicional.jsx
+++ src/nordus/screens/others/AgendamentosAdicional.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, Text, StyleSheet, View, Pressable } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  View,
+  Pressable,
+  ScrollView,
+} from "react-native";
 import Barber from "../../components/Barber";
 import Hour from "../../components/Hour";
 import Calendar from "../../components/Calendar";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getAppointments } from "../../utils/AppointmentService";
 import { FIREBASE_DB } from "../../FirebaseConfig";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where, addDoc, getFirestore } from "firebase/firestore";
 
 export default function AgendamentoAdicional({ duration }) {
   const [barbers, setBarbers] = useState([]);
@@ -61,8 +68,51 @@ export default function AgendamentoAdicional({ duration }) {
     }
   }, [barbeiroEscolhido, data]);
 
+  const qntHorarios = 66;
+  console.log("asa");
+  const HourTwo = 9;
+
+  var min = 0;
+  var hora2 = HourTwo;
+  var agendArray = [];
+
+  for (let i = 0; i < qntHorarios; i++) {
+    let temp = i % 6;
+    min = temp * 10;
+    if (min == 0 && i != 0) {
+      hora2++;
+    }
+    let horacaralho = hora2 + ":" + min;
+    agendArray.push(horacaralho);
+    min = 0;
+  }
+
+  /**
+   * barberID "QZcAbsueFGNLI93VNWOcG72ctbC2" (string)
+
+clientID "IjDHmA1yjaSZ8ropiDGsJYfv6As1" (string)
+
+date "2024/06/20" (string)
+
+hour "12:30" (string)
+
+service /services/01 (reference)
+
+serviceDuration 20 (number)
+
+serviceName "Corte"
+
+status "valid"
+   * 
+   */
+
+  const handleSubmit = (hora) => {
+  addDoc(collection(getFirestore(), "appointments"), { // Adiciona um documento na coleção "users"
+    hour: hora
+  })};
+
   return (
-    <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={{ gap: 12 }}>
         <Text style={{ color: "#fff", fontSize: 24 }}>
           Escolha um profissional:
@@ -106,10 +156,21 @@ export default function AgendamentoAdicional({ duration }) {
           <Text style={{ color: "#fff", fontSize: 20 }}>
             Escolha um horário:{" "}
           </Text>
-          <Hour setHorario={setHorario} />
+          <View>
+            {agendArray.map((agend, index) => (
+              <Pressable
+                key={index}
+                onPress={() => {
+                  handleSubmit(agend);
+                }}
+              >
+                <Text style={styles.text}>{agend}</Text>
+              </Pressable>
+            ))}
+          </View>
         </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 }
 
@@ -117,7 +178,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#1A1E21",
     flex: 1,
-    alignItems: "left",
+    // alignItems: "left",
     paddingTop: 20,
     paddingHorizontal: 30,
     gap: 18,
