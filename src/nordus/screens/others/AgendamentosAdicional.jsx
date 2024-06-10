a216460bf29f50c176ed09ca6c6fb0f6a8d9093e
Anna-Carla <annacarlateixeira16@gmail.com>
2024-06-09 22:53:53 -0300

    feat: update timeSlots

--- src/nordus/screens/others/AgendamentosAdicional.jsx
+++ src/nordus/screens/others/AgendamentosAdicional.jsx
@@ -25,6 +25,7 @@ import { useNavigation } from "@react-navigation/native";
 
 export default function AgendamentoAdicional({
   serviceDuration,
+  setServiceDuration,
   serviceName,
   clientID,
 }) {
@@ -35,6 +36,7 @@ export default function AgendamentoAdicional({
   const status = "valid";
   const [showModal, setShowModal] = useState(false);
   const navigation = useNavigation();
+  const [dayTimeSlots, setDayTimeSlots] = useState(new Map());
 
   useEffect(() => {
     const fetchBarbers = async () => {
@@ -67,26 +69,26 @@ export default function AgendamentoAdicional({
     fetchBarbers();
   }, []);
 
-
-    const fetchAppointments = async (barberId) => {
-      if (barberId) {
-        try {
-          const appointments = await getAppointments(barberId);
-          return(appointments);
-        } catch (error) {
-          console.error("Erro ao obter compromissos:", error);
-        }
+  const fetchAppointments = async (barberId) => {
+    if (barberId) {
+      try {
+        const appointments = await getAppointments(barberId);
+        return appointments;
+      } catch (error) {
+        console.error("Erro ao obter compromissos:", error);
       }
-    };
+    }
+  };
 
   const handleSubmit = async () => {
     if (barbeiroEscolhido && data && horario) {
       let horarioDividido = horario.split(":");
-      let hora = horarioDividido[0];
-      let minuto = horarioDividido[1];
-      data.setHours(hora, minuto);
+      let hora = parseInt(horarioDividido[0]);
+      let minuto = parseInt(horarioDividido[1]);
+      let dataAgendamento = new Date(data);
+      dataAgendamento.setHours(hora, minuto, 0, 0);
 
-      let date = Timestamp.fromDate(new Date(data));
+      let date = Timestamp.fromDate(dataAgendamento);
 
       try {
         await addDoc(collection(getFirestore(), "appointments"), {
@@ -99,6 +101,7 @@ export default function AgendamentoAdicional({
           status: status,
         });
         setShowModal(true);
+        setServiceDuration(null);
         console.log("Agendamento realizado com sucesso.");
       } catch (error) {
         console.error("Erro ao agendar horário:", error);
@@ -115,19 +118,23 @@ export default function AgendamentoAdicional({
     };
 
     if (!showModal) {
-      return null; // Não renderiza nada se showModal for falso
+      return null;
     }
 
     return (
       <View style={[styles.modalContainer]}>
         <View style={styles.modalContent}>
-          <Text style={{ fontSize: 20}}>Agendamento realizado com sucesso!</Text>
+          <Text style={{ fontSize: 20 }}>
+            Agendamento realizado com sucesso!
+          </Text>
           <TouchableHighlight
             underlayColor="#d96541"
             style={styles.buttonModal}
-            onPress={handleReturnHome} // Evento de pressionar para voltar para a página inicial
+            onPress={handleReturnHome}
           >
-            <Text style={styles.buttonTextModal}>Voltar para página Inicial</Text>
+            <Text style={styles.buttonTextModal}>
+              Voltar para página Inicial
+            </Text>
           </TouchableHighlight>
         </View>
       </View>
@@ -139,64 +146,62 @@ export default function AgendamentoAdicional({
   async function escolherBarbeiro() {
     try {
       if (agendamentosPorBarbeiro.has(barbeiroEscolhido.toString())) {
-        console.log('Barbeiro com dados já salvos.');
+        console.log("Barbeiro com dados já salvos.");
       } else {
         const result = await fetchAppointments(barbeiroEscolhido);
-        
+
         result.forEach((appointment) => {
           appointment.date = appointment.date.toDate();
-          console.log("Existe um agendamento: ", appointment.date)
+          console.log("Existe um agendamento: ", appointment.date);
         });
-        console.log('Salvando no hashmap.');
+        console.log("Salvando no hashmap.");
         agendamentosPorBarbeiro.set(barbeiroEscolhido.toString(), result);
       }
       let agg = agendamentosPorBarbeiro.get(barbeiroEscolhido.toString());
-      return agg
-
+      return agg;
     } catch (error) {
-      console.error('Erro ao buscar os agendamentos:', error);
+      console.error("Erro ao buscar os agendamentos:", error);
     }
   }
 
   async function calculaHorariosDisponiveis(dayTimeSlots) {
     let objAppointments = await escolherBarbeiro(barbeiroEscolhido);
 
-      if(objAppointments==undefined || objAppointments == null) {
-        console.log('undefined')
-        return;
-  
-      } else {
-  
-        objAppointments.map((el) => {
-          const data = el.date;
+    if (objAppointments == undefined || objAppointments == null) {
+      console.log("undefined");
+      return;
+    } else {
+      objAppointments.map((el) => {
+        const data = el.date;
 
-          if(dayTimeSlots.has(data.toString())) { 
-            let qntPeriodosMinimos = el.serviceDuration/10;
+        if (dayTimeSlots.has(data.toString())) {
+          let qntPeriodosMinimos = el.serviceDuration / 10;
 
-            for(let i=0; i<qntPeriodosMinimos; i++) { 
-              var qntMinutoPorPeriodo=0;
-              if(i!=0)
-                qntMinutoPorPeriodo = 10;
-                
-              let horaAntiga = data.getHours();
-              let minutoAntiga = data.getMinutes();
+          for (let i = 0; i < qntPeriodosMinimos; i++) {
+            var qntMinutoPorPeriodo = 0;
+            if (i != 0) qntMinutoPorPeriodo = 10;
 
-              let minutoNovo = minutoAntiga+qntMinutoPorPeriodo;
+            let horaAntiga = data.getHours();
+            let minutoAntiga = data.getMinutes();
 
-              if(minutoNovo>59) {
-                let horaNova = Math.floor(minutoNovo/60); 
-                minutoNovo = minutoNovo%60; 
-                data.setHours(horaNova+horaAntiga);
-              } 
+            let minutoNovo = minutoAntiga + qntMinutoPorPeriodo;
 
-              data.setMinutes(minutoNovo);
-                
-              dayTimeSlots.delete(data.toString());
+            if (minutoNovo > 59) {
+              let horaNova = Math.floor(minutoNovo / 60);
+              minutoNovo = minutoNovo % 60;
+              data.setHours(horaNova + horaAntiga);
             }
+
+            data.setMinutes(minutoNovo);
+
+            dayTimeSlots.delete(data.toString());
           }
-        })
-        console.log(dayTimeSlots)
-      }
+        }
+      });
+      console.log(dayTimeSlots);
+      setDayTimeSlots(new Map(dayTimeSlots));
+    }
+    return dayTimeSlots;
   }
 
   const generateDayTimeSlots = (diaEscolhido) => {
@@ -205,26 +210,25 @@ export default function AgendamentoAdicional({
     let min = 0;
     let hour = startHour;
     const slots = new Map();
+    const diaEscolhidoData = new Date(diaEscolhido);
 
     for (let i = 0; i < qntHorarios; i++) {
       min = (i % 6) * 10;
       if (i !== 0 && min === 0) hour++;
-      min = min < 10 ? "0" + min : min;
-      diaEscolhido.setMinutes(min)
-      diaEscolhido.setHours(hour);
-      slots.set(diaEscolhido.toString(), diaEscolhido.toString());
+      const slot = new Date(diaEscolhidoData);
+      slot.setHours(hour, min, 0, 0);
+      slots.set(slot.toString(), slot.toString());
     }
     return slots;
   };
 
   useEffect(() => {
-    if(data!=null) {
-      console.log('verifcando datas')
-      let slots = generateDayTimeSlots(data)
+    if (data != null) {
+      console.log("verifcando datas");
+      let slots = generateDayTimeSlots(data);
       calculaHorariosDisponiveis(slots);
     }
-
-  }, [data])
+  }, [data]);
 
   return (
     <ScrollView style={styles.container}>
@@ -236,8 +240,7 @@ export default function AgendamentoAdicional({
           {barbers.map((barber, index) => (
             <Pressable
               key={index}
-              onPress={() => setBarbeiroEscolhido(barber.id)
-                }
+              onPress={() => setBarbeiroEscolhido(barber.id)}
             >
               <Barber
                 name={barber.name}
@@ -260,18 +263,27 @@ export default function AgendamentoAdicional({
             Escolha um horário:
           </Text>
           <View style={styles.timeSlotsContainer}>
-            {/* {timeSlots.map((slot, index) => (
-              <Pressable
-                key={index}
-                onPress={() => setHorario(slot)}
-                style={[
-                  styles.timeSlot,
-                  horario === slot && styles.timeSlotSelected,
-                ]}
-              >
-                <Text style={styles.timeSlotText}>{slot}</Text>
-              </Pressable>
-            ))} */}
+            {Array.from(dayTimeSlots.values()).map((slot, index) => {
+              const date = new Date(slot);
+              const hour = String(date.getHours()).padStart(2, "0"); // Adiciona zero à esquerda se for menor que 10
+              const minute = String(date.getMinutes()).padStart(2, "0");
+
+              // Formatação da hora e minuto
+              const formattedTime = `${hour}:${minute}`;
+
+              return (
+                <Pressable
+                  key={index}
+                  onPress={() => setHorario(formattedTime)}
+                  style={[
+                    styles.timeSlot,
+                    horario === formattedTime && styles.timeSlotSelected,
+                  ]}
+                >
+                  <Text style={styles.timeSlotText}>{formattedTime}</Text>
+                </Pressable>
+              );
+            })}
           </View>
         </View>
         <View style={styles.handleButton}>
