4f83cdb39fd7ae659a507bd6fb5a93fc86986505
Davi Ribeiro <davirpa1@gmail.com>
2024-06-23 13:28:15 -0300

    fix: updating calendar component
    nesse commit modifiquei o componente calendar para utilizar o moment.js, além disso, agora não é possível selecionar datas anteriores ao dia de hoje, e posteriores à daqui 30 dias

--- src/nordus/components/Calendar.jsx
+++ src/nordus/components/Calendar.jsx
@@ -1,65 +1,37 @@
-import {StyleSheet } from "react-native";
-import { useState } from "react";
 import DatePicker from "react-native-modern-datepicker";
+import moment from "moment";
 
-export default function Calendar({setData}) {
+export default function Calendar({ setData }) {
+  const today = moment();
+  const minimumDate = today.format("YYYY-MM-DD");
 
-    var month = new Date().getMonth()+1;
-    var year = new Date().getFullYear();
-    var todaysDate = new Date(2024, month, 0);
-    var qntDays =  todaysDate.toString().split(' ')[2];
+  const futureDate = moment(today).add(30, "days");
+  const maximumDate = futureDate.format("YYYY-MM-DD");
 
-    function handleDate(dataSelecionada) {
-        let dataArr = dataSelecionada.split('/');
-        let ano = dataArr[0];
-        let mes = dataArr[1];
-        let dia = dataArr[2];
-  
-        let stringData = ano+'-'+mes+'-'+dia;
-        let data = new Date(stringData);
+  function handleDate(dataSelecionada) {
+    const data = moment(dataSelecionada, "YYYY/MM/DD").toDate();
 
-        setData(data);
-        console.log(data);
-    }
+    setData(data);
+    console.log(data);
+  }
 
-    return (
-        <DatePicker
-            mode='calendar'
-            minimumDate={year+"/"+month+"/01"}
-            maximumDate={year+"/"+month+"/"+qntDays}
-            style={{borderRadius: 20}}
-            onDateChange={handleDate}
-            onDisabledDayError={() => console.log("hey")}
-            options={{
-                backgroundColor: '#353535',
-                textHeaderColor: '#fff',
-                textDefaultColor: '#fff',
-                selectedTextColor: '#fff',
-                mainColor: '#EA714C',
-                textSecondaryColor: '#909090',
-                borderColor: 'rgba(122, 146, 165, 0.1)',
-            }}
-        /> 
-    )
+  return (
+    <DatePicker
+      mode="calendar"
+      minimumDate={minimumDate}
+      maximumDate={maximumDate}
+      style={{ borderRadius: 20 }}
+      onDateChange={handleDate}
+      onDisabledDayError={() => console.log("hey")}
+      options={{
+        backgroundColor: "#353535",
+        textHeaderColor: "#fff",
+        textDefaultColor: "#fff",
+        selectedTextColor: "#fff",
+        mainColor: "#EA714C",
+        textSecondaryColor: "#909090",
+        borderColor: "rgba(122, 146, 165, 0.1)",
+      }}
+    />
+  );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center', 
-        alignItems: 'center',
-        backgroundColor: '#353535',
-        height: 130,
-        borderRadius: 20
-    },
-    image: {
-        width: 70, 
-        height:70, 
-        borderRadius: 100
-    },
-    text: {
-        color: '#fff',
-    },
-    item: {
-        justifyContent:'center',
-    }
-});
