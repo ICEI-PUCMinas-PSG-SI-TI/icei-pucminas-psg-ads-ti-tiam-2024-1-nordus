6a9d2da695f8a534043604453d3399ac2235f21a
AR3X <alexgdiniz18@gmail.com>
2024-06-23 11:29:57 -0300

    Rota Criada

--- src/nordus/App.js
+++ src/nordus/App.js
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Routes } from "./routes/index";
+import { Routes, RoutesBarber } from "./routes/index";
+import { getUser} from "./utils/UserService";
+
 
 
 export default function App() {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [isBarber, setIsBarber] = useState(false);
 
-  return (
-    <Routes isUserLoggedIn={isUserLoggedIn} setIsUserLoggedIn={setIsUserLoggedIn} />
-  );
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const userData = await getUser();
+        setUser(userData);
+        setIsBarber(userData.barber)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (isUserLoggedIn) {
+      fetchUser();
+    }
+  }, [isUserLoggedIn]);
+
+  
+
+  return(
+   <Routes isUserLoggedIn={isUserLoggedIn} setIsUserLoggedIn={setIsUserLoggedIn} isBarber={isBarber}/>
+  )
+
 }
 
